e88ec69f4a1b1dae105b30da61c7641b44a7f9d1
Josiel Jr. <josieljrr.dev@gmail.com>
2024-07-10 14:39:11 -0300

    Adiciona conteúdo a pasta manipulando_DOM

--- 7_manipulando_DOM/script.js
+++ 7_manipulando_DOM/script.js
@@ -186,3 +186,84 @@
 
 // ----------------------------------------------------------------------------------------------------------------------
 
+// IMPLEMENTANDO EVENTOS:
+
+// Evento Onload:
+
+// window.onload = function () {
+//     console.log('Carregou o DOM');
+// };
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Evento de click:
+
+// const botao = document.querySelector('#btn');
+
+// // Adiciona um event listener para o evento 'click' no botão
+// botao.addEventListener('click', function () {
+//     console.log('Clicou');
+//     this.textContent = 'Botão Clicado';
+//     this.style.cssText = 'color: green; background-color: red;';
+// });
+
+// // Click afetando outros elementos: 
+// const titulo = document.querySelector('#title');
+// titulo.addEventListener('click', () => {
+//     const subtitulo = document.querySelector('.subtitle');
+//     subtitulo.style.display = 'none';
+// });
+
+// // Duble click:
+// const paragrafo = document.querySelector('#paragrafo');
+// paragrafo.addEventListener('dblclick', () => {
+//     paragrafo.style.color = 'green';
+// });
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Eventos mouseover e mouseout:
+
+// // Seleciona o elemento HTML com o id 'title'
+// const title = document.querySelector('#title');
+
+// // Adiciona um evento 'mouseover' ao elemento 'title'
+// title.addEventListener('mouseover', () => title.style.backgroundColor = 'pink');
+
+// // Adiciona um evento 'mouseout' ao elemento 'title'
+// title.addEventListener('mouseout', () => title.style.backgroundColor = 'white');
+
+
+// const subtitle = document.querySelector('.subtitle');
+
+// subtitle.addEventListener('mouseover', () => {
+//     const legenda = document.querySelector('#legenda');
+//     legenda.classList.remove('legenda');
+// });
+
+// subtitle.addEventListener('mouseout', () => {
+//     const legenda = document.querySelector('#legenda');
+//     legenda.classList.add('legenda');
+// });
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Eventos keydown e keyup:
+
+// // Adiciona um evento 'keydown' ao documento
+// document.addEventListener('keydown', (e) => {
+//     // Verifica se a tecla pressionada é 'Enter'
+//     if (e.key === 'Enter') {
+//         console.log('Apertou o Enter');
+//     }
+// });
+
+// // Adiciona um evento 'keyup' ao documento
+// document.addEventListener('keyup', (e) => {
+//     // Verifica se a tecla liberada é 'Enter'
+//     if (e.key === 'Enter') {
+//         console.log('Soltou o Enter');
+//     }
+// });
+
+// ----------------------------------------------------------------------------------------------------------------------
