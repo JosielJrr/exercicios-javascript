815a89865220f192a06a383c877e1cc8070ed603
Josiel Jr. <josieljrr.dev@gmail.com>
2024-07-15 15:46:50 -0300

    Adiciona arquivo ternaria.js na pasta de estruturas condicionais

---
+++ 1_estruturas_condicionais/ternaria.js
@@ -0,0 +1,32 @@
+// Verificação de Paridade:
+
+// function verificaParidade(num) {
+
+//     num % 2 === 0 ? console.log(`${num} é Par`) : console.log(`${num} é Ímpar`);
+// }
+
+// verificacaoParidade(9);
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Verificação de Maioridade:
+
+// function verificaMaiorIdade(idade) {
+
+//     idade >= 18 ? console.log('Maior de idade') : console.log('Menor de idade');
+// }
+
+// verificaMaiorIdade(10);
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Avaliação de Nota:
+
+// function verificaNota(nota) {
+
+//     nota >= 6 ? console.log('APROVADO') : console.log('REPROVADO');
+// }
+
+// verificaNota(9);
+
+// ----------------------------------------------------------------------------------------------------------------------
